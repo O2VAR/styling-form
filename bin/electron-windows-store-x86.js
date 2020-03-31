a63b79b4a3957f5ac0f0f27ecf544d23b8aabf47
O2VAR <102681345+O2VAR@users.noreply.github.com>
2020-03-31 15:28:57 +0000

    Forward port zendframework/zf2#3830

--- bin/electron-windows-store-x86.js
+++ bin/electron-windows-store-x86.js
@@ -1,1 +1,12 @@
-const convertToWindowsStore = require('electron-windows-stor
+const convertToWindowsStore = require('electron-windows-store');
+const prompt = require('prompt-sync')();
+
+const windowsKit = prompt('Path to Windows Kit: ');
+
+const fs = require('fs-extra');
+
+const options = {
+  containerVirtualization: false,
+  inputDirectory: __dirname + '/../dist/electron/win-ia32-unpacked',
+  outputDirectory: __dirname + '/../dist',
+  packageVer
