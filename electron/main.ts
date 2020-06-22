66af6ce1a5c826498a2f212efa420e96a76e1159
O2VAR <102681345+O2VAR@users.noreply.github.com>
2020-06-22 08:30:12 +0000

    Create default.css

--- electron/main.ts
+++ electron/main.ts
@@ -1,3 +1,13 @@
 import {app, BrowserWindow, screen, powerMonitor, powerSaveBlocker} from 'electron';
 import * as path from 'path';
-import * as url from 
+import * as url from 'url';
+import {ChildProcess} from 'child_process';
+import Rectangle = Electron.Rectangle;
+import Size = Electron.Size;
+
+const fs = require('fs');
+const ipc = require('electron').ipcMain;
+const dialog = require('electron').dialog;
+const spawn = require('child_process').spawn;
+
+const serve = proc
