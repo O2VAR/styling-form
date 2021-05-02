91e24aca3166bd5a5e8f7fbaef5e08a3072d063c
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-05-02 12:20:24 +0000

    Modal

--- src/main/scala/net/creasource/Main.scala
+++ src/main/scala/net/creasource/Main.scala
@@ -2,4 +2,14 @@ package net.creasource
 
 import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.server.Directives._
-import akka.http
+import akka.http.scaladsl.server.Route
+import net.creasource.core.Application
+import net.creasource.http.{SPAWebServer, SocketWebServer}
+import net.creasource.web._
+
+import scala.io.StdIn
+
+/**
+  * The Main class that bootstraps the application.
+  */
+object Main extends App with SPAWebServer with SocketWebServer {
