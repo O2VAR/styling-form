c6bc6dc1ec8c72b7679a3e3c8b78adb9f3f858b1
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-05-09 15:55:44 +0000

    Moving files

--- src/main/scala/net/creasource/core/Application.scala
+++ src/main/scala/net/creasource/core/Application.scala
@@ -2,4 +2,18 @@ package net.creasource.core
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.ActorMaterializer
-import com.typesafe.config.{Config, Config
+import com.typesafe.config.{Config, ConfigFactory}
+import net.creasource.web.{LibraryActor, LyricsActor, SettingsActor}
+
+import scala.concurrent.Await
+
+object Application {
+
+  def apply() = new Application
+
+}
+
+/**
+  * Represents an application. This is where you'll instantiate your top actors, connect to a database, etc...
+  */
+class App
