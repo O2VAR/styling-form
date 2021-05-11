6aafd4a8ab823bb699a34beb42a6bd1983bcb7bd
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-05-11 11:57:00 +0000

    Update version to 0.7.0

--- src/main/scala/net/creasource/http/SocketWebServer.scala
+++ src/main/scala/net/creasource/http/SocketWebServer.scala
@@ -4,4 +4,11 @@ import akka.Done
 import akka.actor.{ActorRef, Props, Status}
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
-import akka.h
+import akka.http.scaladsl.server.Route
+import akka.pattern.ask
+import akka.stream.{KillSwitches, OverflowStrategy, SharedKillSwitch}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import net.creasource.http.actors.{SocketSinkActor, 
