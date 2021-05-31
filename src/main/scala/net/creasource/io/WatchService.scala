787eb46d01a7d58771b0728f294c2d68671825ad
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-05-31 11:22:15 +0000

    add dep

--- src/main/scala/net/creasource/io/WatchService.scala
+++ src/main/scala/net/creasource/io/WatchService.scala
@@ -3,4 +3,14 @@ package net.creasource.io
 /**
   * This file is copy/pasted from https://github.com/nurkiewicz/learning-akka
   * It is released under the Apache 2 License (https://github.com/nurkiewicz/learning-akka/blob/master/license.txt)
-  * Substantial modifications have been
+  * Substantial modifications have been made, including changing the package name, and fixing the code for fast copy.
+  */
+
+import java.io.UncheckedIOException
+import java.nio.file.StandardWatchEventKinds._
+import java.nio.file._
+
+import akka.Done
+import akka.actor.ActorRef
+import akka.event.LoggingAdapter
+import 
