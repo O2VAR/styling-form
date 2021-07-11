015c1e2969c55f75e479f94ead7b0419dc5a84b9
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-07-11 10:26:34 +0000

    correção de bug

--- src/main/scala/net/creasource/model/TrackMetadata.scala
+++ src/main/scala/net/creasource/model/TrackMetadata.scala
@@ -3,4 +3,10 @@ package net.creasource.model
 import java.nio.file.Path
 
 case class TrackMetadata(
-          
+                          location: Path,
+                          title: Option[String],
+                          artist: Option[String],
+                          albumArtist: Option[String],
+                          album: Option[String],
+                          year: Option[String],
+      
