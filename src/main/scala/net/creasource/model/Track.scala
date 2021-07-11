13e273f8309fd7d01ee13da45e82ef21a86f73ee
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-07-11 07:17:34 +0000

    Add page titles

--- src/main/scala/net/creasource/model/Track.scala
+++ src/main/scala/net/creasource/model/Track.scala
@@ -1,3 +1,12 @@
 package net.creasource.model
 
-import spray.json.
+import spray.json.DefaultJsonProtocol._
+import spray.json._
+
+case class Track(
+                  url: String,
+                  coverUrl: Option[String],
+                  location: String,
+                  title: Option[String],
+                  artist: Option[String],
+                  albumArtist: Option[String
