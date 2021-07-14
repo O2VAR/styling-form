607f5f1a8298d5996e490e38681fd9f0d043cceb
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-07-14 10:14:09 +0000

    Final Fixes

--- src/main/scala/net/creasource/web/AudioLibraryRoutes.scala
+++ src/main/scala/net/creasource/web/AudioLibraryRoutes.scala
@@ -7,4 +7,13 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.settings.RoutingSettings
 import akka.pattern.ask
-import net.creasource.core.Applicatio
+import net.creasource.core.Application
+import net.creasource.web.LibraryActor.{GetLibraries, Libraries}
+
+import scala.concurrent.duration._
+
+class AudioLibraryRoutes(application: Application) {
+
+  implicit val settings: RoutingSettings = RoutingSettings.apply(application.config)
+
+  implicit val askTimeout: akka.util.Timeout = 2.sec
