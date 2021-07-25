3838750a910e7c43654e16b4b6a8915d90f68953
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-07-25 10:09:20 +0000

    removed obsolete class

--- src/main/scala/net/creasource/web/LyricsActor.scala
+++ src/main/scala/net/creasource/web/LyricsActor.scala
@@ -1,2 +1,108 @@
 
 package net.creasource.web
+
+import java.io.{FileInputStream, FileOutputStream}
+import java.nio.file.{Files, Path, Paths}
+
+import akka.Done
+import akka.actor.{Actor, Props}
+import akka.event.Logging
+import akka.stream.scaladsl.{Sink, Source, StreamConverters}
+import akka.util.ByteString
+import net.creasource.core.Application
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object LyricsActor {
+
+  case class GetLyrics(artist: String, title: String)
+
+  sealed trait GetLyricsResult
+  case class LyricsFound(lyrics: String) extends GetLyricsResult
+  case object LyricsNotFound extends GetLyricsResult
+
+  case class SaveLyrics(lyrics: String, artist: String, title: String)
+
+  sealed trait SaveLyricsResult
+  case object LyricsSaved extends SaveLyricsResult
+  case class LyricsSaveError(error: String) extends SaveLyricsResult
+
+  case object DeleteLyrics
+
+  def props()(implicit application: Application): Props = Props(new LyricsActor())
+
+}
+
+class LyricsActor()(implicit application: Application) extends Actor with JsonSupport {
+
+  import LyricsActor._
+  import application.materializer
+  import context.dispatcher
+
+  private val logger = Logging(context.system, this)
+
+  val cacheFolder: Path = Paths.get(application.config.getString("music.cacheFolder"))
+  val lyricsFolder: Path = cacheFolder.resolve("lyrics")
+
+  override def preStart(): Unit = {
+    lyricsFolder.toFile.mkdirs()
+  }
+
+  override def receive: Receive = {
+
+    case GetLyrics(artist, title) =>
+      val client = sender()
+      loadLyrics(artist, title).onComplete{
+        case Success(lyrics) => client ! LyricsFound(lyrics)
+        case Failure(_) => client ! LyricsNotFound
+      }
+
+    case SaveLyrics(lyrics, artist, title) =>
+      val client = sender()
+      this.saveLyrics(lyrics, artist, title).onComplete {
+        case Success(Done) => client ! LyricsSaved
+        case Failure(t) => client ! LyricsSaveError(t.getMessage)
+      }
+
+    case DeleteLyrics =>
+      logger.info("Deleting lyrics")
+      Files.walk(lyricsFolder)
+        .filter(path => path != lyricsFolder)
+        .forEach(path => path.toFile.delete())
+      sender() ! Done
+
+  }
+
+  def loadLyrics(artist: String, title: String): Future[String] = {
+    val lyricsFile = lyricsFolder.resolve(toCompressedString(artist) + "-" + toCompressedString(title) + ".txt").toFile
+    StreamConverters
+      .fromInputStream(() => new FileInputStream(lyricsFile))
+      .map(_.utf8String)
+      .runWith(Sink.head)
+  }
+
+  def saveLyrics(lyrics: String, artist: String, title: String): Future[Done] = {
+    val lyricsFile = lyricsFolder.resolve(toCompressedString(artist) + "-" + toCompressedString(title) + ".txt").toFile
+    Source.single(ByteString(lyrics))
+      .runWith(StreamConverters.fromOutputStream(() => new FileOutputStream(lyricsFile), autoFlush = true))
+      .transform {
+        case Success(result) => result.status
+        case Failure(t) => Failure(t)
+      }
+  }
+
+  def toCompressedString(s: String): String = {
+    val compressed = new StringBuffer
+    var i = 0
+    while (i < s.length) {
+      val ch = s.charAt(i)
+      if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || (ch == '&') || (ch == '+') || (ch == '(') || (ch == ')'))
+        compressed.append(ch)
+      i += 1
+    }
+    val res = compressed.toString
+    res.substring(0, scala.math.min(res.length, 50))
+  }
+
+}
---
+++ src/main/scala/net/creasource/web/SettingsActor.scala
@@ -0,0 +1,54 @@
+
+package net.creasource.web
+
+import java.net.NetworkInterface
+import java.nio.file.{Files, Path, Paths}
+
+import akka.Done
+import akka.actor.{Actor, Props}
+import akka.event.Logging
+import net.creasource.core.Application
+import net.creasource.web.SettingsActor.{DeleteCovers, GetHostIps}
+
+import scala.collection.JavaConverters._
+
+object SettingsActor {
+
+  case object GetHostIps
+  case object DeleteCovers
+
+  def props()(implicit application: Application): Props = Props(new SettingsActor())
+
+}
+
+class SettingsActor()(implicit application: Application) extends Actor with JsonSupport {
+
+  private val logger = Logging(context.system, this)
+
+  val cacheFolder: Path = Paths.get(application.config.getString("music.cacheFolder"))
+
+  override def receive: Receive = {
+
+    case GetHostIps =>
+      val interfaces: Seq[NetworkInterface] = java.net.NetworkInterface.getNetworkInterfaces.asScala.toSeq
+      val ipAddresses: Seq[String] =
+        interfaces.flatMap { p =>
+          logger.debug("Found interface: " + p.getDisplayName)
+          p.getInetAddresses.asScala.toSeq
+        }.collect { case address if !address.isLoopbackAddress && address.getHostAddress.contains(".") =>
+          logger.debug("Found address: " + address.getHostAddress)
+          address.getHostAddress
+        }
+      sender() ! ipAddresses
+
+    case DeleteCovers =>
+      logger.info("Deleting covers")
+      val covers = cacheFolder.resolve("covers")
+      Files.walk(covers)
+        .filter(path => path != covers)
+        .forEach(path => path.toFile.delete())
+      sender() ! Done
+
+  }
+
+}
