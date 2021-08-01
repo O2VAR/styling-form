f5670a7bf455d60507ee23d7e02bf90cb8ec64c7
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-08-01 13:26:41 +0000

    v0.8

--- src/main/scala/net/creasource/web/package.scala
+++ src/main/scala/net/creasource/web/package.scala
@@ -1,1 +1,14 @@
 package net.creasource
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model._
+import akka.util.ByteString
+import spray.json._
+
+import scala.util.Try
+
+package object web {
+
+  case class JsonMessage(method: String, id: Int, entity: JsValue)
+
+  object Json
