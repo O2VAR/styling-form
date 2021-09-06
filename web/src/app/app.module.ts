74a1e411afe37c8ab3c7ce98f61d47598ebe5357
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-06 13:53:32 +0000

    fixes #2

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,4 +3,12 @@ import {RouterModule} from '@angular/router';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from '@ngrx/effects';
-import {StoreDevtools
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+
+import {routes} from './routes';
+
+import {CoreModule} from './core/core.module';
+import {CoreComponent} from './core/core.component';
+import {LibraryModule} from './library/library.module';
+import {
