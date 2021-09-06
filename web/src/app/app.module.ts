e4a63258668b0f827a3cda1047b49f4c4f861f60
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-06 09:25:25 +0000

    Syntax updates

---
+++ web/src/app/app.module.ts
@@ -0,0 +1,6 @@
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtools
