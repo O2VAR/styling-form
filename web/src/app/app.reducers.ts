30d0199093447bf810376132ef1da518fd2b83f8
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-07 12:31:27 +0000

    updating package.json

--- web/src/app/app.reducers.ts
+++ web/src/app/app.reducers.ts
@@ -4,4 +4,13 @@ import {routerReducer, RouterReducerState} from '@ngrx/router-store';
 import {RouterStateUrl} from '@app/app.serializer';
 
 export interface State {
-  router: RouterReducerS
+  router: RouterReducerState<RouterStateUrl>;
+}
+
+/**
+ * Our state is composed of a map of action reducer functions.
+ * These reducer functions are called with each dispatched action
+ * and the current or initial state and return a new immutable state.
+ */
+export const reducers: ActionReducerMap<State> = {
+  router: routerReduc
