fd11dd6b1ff208420011100ba57202651fbbff2d
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-07 11:38:58 +0000

    Updated Travis CI config

--- web/src/app/app.reducers.ts
+++ web/src/app/app.reducers.ts
@@ -1,1 +1,7 @@
-import {ActionReducerM
+import {ActionReducerMap, createFeatureSelector, createSelector, MetaReducer} from '@ngrx/store';
+import {environment} from '@env/environment';
+import {routerReducer, RouterReducerState} from '@ngrx/router-store';
+import {RouterStateUrl} from '@app/app.serializer';
+
+export interface State {
+  router: RouterReducerS
