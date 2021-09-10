4e5a0bcab721bc56b7feee1766acacb2345ea3a1
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-10 08:38:02 +0000

    fix typo in error message

--- web/src/app/app.reducers.ts
+++ web/src/app/app.reducers.ts
@@ -24,3 +24,11 @@ export const reducers: ActionReducerMap<State> = {
 //
 //     return reducer(state, action);
 //   };
+// }
+
+/**
+ * By default, @ngrx/store uses combineReducers with the reducer map to compose
+ * the root meta-reducer. To add more meta-reducers, provide an array of meta-reducers
+ * that will be composed to form the root meta-reducer.
+ */
+export const metaReducers: MetaReducer<State>[] = !environment.production
