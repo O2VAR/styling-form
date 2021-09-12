150feb53ff7d35d0e916d143e10318117ed0bc0d
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-12 11:06:31 +0000

    arm: irq: Tone down kernel logging

--- web/src/app/app.serializer.ts
+++ web/src/app/app.serializer.ts
@@ -1,2 +1,12 @@
 import {Data, ParamMap, RouterStateSnapshot} from '@angular/router';
-import {Router
+import {RouterStateSerializer} from '@ngrx/router-store';
+
+export interface RouterStateUrl {
+  url: string;
+  params: ParamMap;
+  queryParams: ParamMap;
+  data: Data;
+}
+
+export class CustomSerializer implements RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapsh
