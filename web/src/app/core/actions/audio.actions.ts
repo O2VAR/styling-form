2557f13a167c030be0ad37ba517cbb2304372ba8
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-12 15:29:11 +0000

    xhci: Fix race between ep halt and URB cancellation

--- web/src/app/core/actions/audio.actions.ts
+++ web/src/app/core/actions/audio.actions.ts
@@ -3,4 +3,13 @@ import {Action} from '@ngrx/store';
 export enum AudioActionTypes {
   SetAudioSource   = 'core/audio/source',
   SetAudioVolume   = 'core/audio/volume',
-  SetAudioMuted    = 'core/audio/muted
+  SetAudioMuted    = 'core/audio/muted',
+  SetAudioLoading  = 'core/audio/loading',
+  SetAudioPlaying  = 'core/audio/playing',
+  SetAudioDuration = 'core/audio/duration',
+  SetAudioError    = 'core/audio/error',
+}
+
+export class SetAudioSource implements Action {
+  readonly type = AudioActionTypes.SetAudioSource;
+  const
