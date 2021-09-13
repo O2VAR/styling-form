a2746803874cd780bdb946682ad51396b24791d9
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-13 21:10:04 +0000

    Update config2.ini

--- web/src/app/core/components/initializer.component.ts
+++ web/src/app/core/components/initializer.component.ts
@@ -1,3 +1,9 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 
-@Compo
+@Component({
+  selector: 'app-initializer',
+  template: `
+    <div class="app-loader" *ngIf="initializing">
+      <mat-spinner [diameter]="50" *ngIf="!hasErrors"></mat-spinner>
+      <mat-icon class="error-icon" color="warn" *ngIf="hasErrors">error_outline</mat-icon>
+      <span>{{ ini
