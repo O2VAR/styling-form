6e91a016e678a6a271e2ef924ce81f665970eb71
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-13 09:14:55 +0000

    updated dependency versions

--- web/src/app/core/actions/core.actions.ts
+++ web/src/app/core/actions/core.actions.ts
@@ -2,4 +2,15 @@ import {Action} from '@ngrx/store';
 import {Theme} from '@app/core/core.utils';
 
 export enum CoreActionTypes {
-  OpenSidena
+  OpenSidenav   = 'core/sidenav/open',
+  CloseSidenav  = 'core/sidenav/close',
+  ToggleSidenav = 'core/sidenav/toggle',
+  ChangeTheme   = 'core/theme',
+}
+
+export class OpenSidenav implements Action {
+  readonly type = CoreActionTypes.OpenSidenav;
+}
+
+export class CloseSidenav implements Action {
+  readonly type
