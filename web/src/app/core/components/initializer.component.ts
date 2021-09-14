5a8ea8eed5690333653cec30cfb8c8b1b93775f1
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-14 15:17:32 +0000

    Update documentation/MicroControllerUnit.md

--- web/src/app/core/components/initializer.component.ts
+++ web/src/app/core/components/initializer.component.ts
@@ -6,4 +6,16 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
     <div class="app-loader" *ngIf="initializing">
       <mat-spinner [diameter]="50" *ngIf="!hasErrors"></mat-spinner>
       <mat-icon class="error-icon" color="warn" *ngIf="hasErrors">error_outline</mat-icon>
-      <span>{{ ini
+      <span>{{ initializingLog }}</span>
+      <span *ngIf="hasErrors" class="retry" (click)="retry.emit()">Retry</span>
+      <span *ngIf="!hasErrors" class="filler">&nbsp;</span>
+    </div>
+  `,
+  styles: [`
+    :host-context(.electron) .app-loader {
+      top: 34px;
+    }
+    .app-loader {
+      position: absolute;
+      left: 0;
+      r
