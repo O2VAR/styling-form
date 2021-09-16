43e827e85ebd140612ffad293b60a38cd8bfb2fb
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-09-16 17:11:33 +0000

    Update aside.html

--- web/src/app/core/components/toolbar.component.ts
+++ web/src/app/core/components/toolbar.component.ts
@@ -1,2 +1,10 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
-import {DomSanitizer} from '@
+import {DomSanitizer} from '@angular/platform-browser';
+import {Theme} from '@app/core/core.utils';
+
+@Component({
+  selector: 'app-toolbar',
+  template: `
+    <mat-toolbar>
+      <button mat-icon-button (click)="toggleSidenav.emit()" class="toggle">
+        <mat-icon>{{ sideNavOpened ? 'close' : 'menu' }}</mat-ic
