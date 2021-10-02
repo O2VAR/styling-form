c628482019d7cadbd21968069b0cb77a792b19d7
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-10-02 16:34:36 +0000

    Minor comment update.

--- web/src/app/core/services/electron.service.ts
+++ web/src/app/core/services/electron.service.ts
@@ -1,2 +1,14 @@
 import {Injectable} from '@angular/core';
-import
+import {environment} from '@env/environment';
+
+@Injectable()
+export class ElectronService {
+
+  private ipcRenderer: any;
+  private remote: any;
+  private shell: any;
+
+  constructor() {
+    if (environment.electron) {
+      const electron = (<any>window).require('electron');
+      this.ipcRenderer = electron.ip
