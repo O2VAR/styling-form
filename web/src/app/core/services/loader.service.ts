7b8b6ef58c76d7a56f6abdc4368809753e2ff357
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-10-18 11:40:28 +0000

    Update lesson.csv

--- web/src/app/core/services/loader.service.ts
+++ web/src/app/core/services/loader.service.ts
@@ -2,4 +2,16 @@ import {Injectable} from '@angular/core';
 import {concat, EMPTY, Observable, Subject, throwError} from 'rxjs';
 import {catchError, delay, mergeMap, publishReplay, refCount, retryWhen, share, switchMap, take} from 'rxjs/operators';
 import {HttpSocketClientService, SocketMessage} from '@app/core/services/http-socket-client.service';
-import {MatSnackBar} from '@angular/mater
+import {MatSnackBar} from '@angular/material';
+
+@Injectable()
+export class LoaderService {
+
+  private readonly loadings$: Observable<number>;
+  private loading = new Subject<number>();
+
+  hasErrors$: Observable<boolean>;
+  private hasErrors = new Subject<boolean>();
+
+  log$: Observable<string>;
+  priv
