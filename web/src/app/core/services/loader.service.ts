c93335dc5a711df88d180c635c75951d0f6afb96
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-10-18 07:52:30 +0000

    Fix typo in variable name.

--- web/src/app/core/services/loader.service.ts
+++ web/src/app/core/services/loader.service.ts
@@ -1,2 +1,5 @@
 import {Injectable} from '@angular/core';
-import {concat, EMPTY, Observable, Subject, throw
+import {concat, EMPTY, Observable, Subject, throwError} from 'rxjs';
+import {catchError, delay, mergeMap, publishReplay, refCount, retryWhen, share, switchMap, take} from 'rxjs/operators';
+import {HttpSocketClientService, SocketMessage} from '@app/core/services/http-socket-client.service';
+import {MatSnackBar} from '@angular/mater
