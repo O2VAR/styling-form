78bc6ea46b33c45644f0e5c85b7f2b8f0174a7fa
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-10-26 21:42:11 +0000

    Changed method name

--- web/src/app/core/services/router.service.ts
+++ web/src/app/core/services/router.service.ts
@@ -1,1 +1,12 @@
-import {Injectable} from '@ang
+import {Injectable} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+
+import * as fromRoot from '@app/app.reducers';
+import {RouterStateUrl} from '@app/app.serializer';
+import {getRouterState} from '@app/app.reducers';
+
+@Injectable()
+export class RouterService {
+
+  construct
