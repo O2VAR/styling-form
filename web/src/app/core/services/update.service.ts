63eaca1d1f615e70e7a65f99b4bea6d55f63a71d
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-11-01 12:30:57 +0000

    v0.9.8

--- web/src/app/core/services/update.service.ts
+++ web/src/app/core/services/update.service.ts
@@ -1,3 +1,13 @@
 import {Injectable} from '@angular/core';
 import {SwUpdate} from '@angular/service-worker';
-import {MatDialog} from '@angular/material
+import {MatDialog} from '@angular/material';
+import {ConfirmComponent} from '@app/shared/dialogs/confirm.component';
+
+@Injectable()
+export class UpdateService {
+
+  constructor(private updates: SwUpdate, private dialog: MatDialog) {}
+
+  initialize(): void {
+    this.updates.available.subscribe(event => {
+      // console.log('current version 
