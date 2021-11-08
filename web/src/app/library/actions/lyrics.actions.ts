dcb9a71dc8ea819cbe35bd153d93746a67865d87
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-11-08 19:56:35 +0000

    Fixing compilation errors

--- web/src/app/library/actions/lyrics.actions.ts
+++ web/src/app/library/actions/lyrics.actions.ts
@@ -1,1 +1,11 @@
-import {Action} from
+import {Action} from '@ngrx/store';
+import {Track} from '@app/model';
+
+export enum LyricsActionTypes {
+  LoadLyrics        = 'library/lyrics/load',
+  LoadLyricsSuccess = 'library/lyrics/load/success',
+  LoadLyricsFailure = 'library/lyrics/load/failure',
+}
+
+export class LoadLyrics implements Action {
+  readonly type = LyricsActionTypes.LoadLyrics;
