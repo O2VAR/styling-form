c66acfd4af03d4839a485541c7eb7afe85042f80
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-11-13 07:18:44 +0000

    simplify the code

--- web/src/app/library/actions/tracks.actions.ts
+++ web/src/app/library/actions/tracks.actions.ts
@@ -5,4 +5,14 @@ export enum TracksActionTypes {
   AddTracks         = 'library/tracks/add',
   RemoveTracks      = 'library/tracks/remove',
   LoadTracks        = 'library/tracks/load',
-  LoadTracksSuccess = '
+  LoadTracksSuccess = 'library/tracks/load/success',
+  LoadTracksFailure = 'library/tracks/load/failure',
+  ScanTracks        = 'library/tracks/scan',
+}
+
+export class AddTracks implements Action {
+  readonly type = TracksActionTypes.AddTracks;
+  constructor(public payload: Track[]) {}
+}
+
+export class RemoveTracks 
