4c8ed44fad6394162eb6aa5d69df08107c922dd6
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-12-22 19:42:57 +0000

    Suppress warning

--- web/src/app/library/components/player/progress.component.ts
+++ web/src/app/library/components/player/progress.component.ts
@@ -1,1 +1,11 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, 
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-progress',
+  template: `
+    <div class="progress">
+      <span class="time-elapsed">{{ currentTime ? (currentTime | sgTime) : '00:00' }}</span>
+      <mat-progress-bar
+        *ngIf="loading"
+        mode="indeterminate"></mat-progress-bar>
+  
