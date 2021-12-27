0b965a36791da4232b246a499f1311cf5ebd9f48
O2VAR <102681345+O2VAR@users.noreply.github.com>
2021-12-27 14:03:23 +0000

    Mac build fix.

--- web/src/app/library/components/shared/list-item.component.ts
+++ web/src/app/library/components/shared/list-item.component.ts
@@ -16,4 +16,9 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
         <mat-icon *ngIf="!avatarStyle">music_note</mat-icon>
       </div>
 
-      <div ma
+      <div matLine class="primary-text">
+        <mat-icon class="warn" *ngIf="warn" color="warn" matTooltip="Unset tags!">warning</mat-icon>
+        <span class="item-name" [innerHtml]="primaryHTML" [matTooltip]="tooltip"></span>
+      </div>
+
+      <div matLine class="secondary-text" [innerHtml]="
