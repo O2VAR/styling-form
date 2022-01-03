cdde8f79c9b9944e35c4f4d76437207c8f964c8e
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-01-03 12:09:51 +0000

    Markups.

--- web/src/app/library/components/shared/loader.component.ts
+++ web/src/app/library/components/shared/loader.component.ts
@@ -4,4 +4,18 @@ import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
   selector: 'app-loader',
   template: `
     <div *ngIf="show">
-      
+      <mat-spinner diameter="30"></mat-spinner>
+      Loading...
+    </div>
+  `,
+  styles: [`
+    div {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      overflow-y: scroll;
+    }
+  `],
+  changeDetection: ChangeDetectionStrate
