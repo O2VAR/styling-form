dd6c82f331acff9f7fcc8a3a3393f0ff032cf913
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-01-05 10:08:03 +0000

    Maven artifacts for Messaging version 1.1.6

--- web/src/app/library/components/tracks.component.ts
+++ web/src/app/library/components/tracks.component.ts
@@ -63,3 +63,161 @@ import {LibraryService} from '@app/library/services/library.service';
                      (addToFavorites)="addToFavorites(track)"
                      (removeFromFavorites)="removeFromFavorites(track)">
           </app-track>
+        </mat-list>
+      </div>
+
+      <app-dictionary *ngIf="sortedAlphabetically" (letterClicked)="scrollTo($event)"></app-dictionary>
+
+    </div>
+  `,
+  styles: [`
+    .wrapper {
+      padding-top: 60px;
+      height: 100%;
+      box-sizing: border-box;
+    }
+    .list-wrapper {
+      overflow-y: scroll;
+      overflow-x: hidden;
+      height: 100%;
+    }
+    .list {
+      padding-top: 0;
+    }
+    .list.sorted-alpha {
+      padding-right: 1rem;
+    }
+    .select-text {
+      font-weight: 300;
+    }
+  `],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TracksComponent implements OnChanges {
+
+  @Output() next: EventEmitter<void> = new EventEmitter();
+  @Output() previous: EventEmitter<void> = new EventEmitter();
+
+  @Input() private tracks: Track[];
+  @Input() currentTrack: Track;
+
+  filteredTracks: Track[];
+
+  @ViewChild('list') list: ElementRef;
+  @ViewChild('tracksMenu') tracksMenu: MatMenu;
+
+  private _search = '';
+  get search() {
+    return this._search;
+  }
+  set search(val: string) {
+    this._search = val;
+    this.filteredTracks = this.sort(this.tracks.filter(this.filter));
+  }
+
+  private _sortedAlphabetically = false;
+  get sortedAlphabetically() {
+    return this._sortedAlphabetically;
+  }
+  set sortedAlphabetically(val: boolean) {
+    this._sortedAlphabetically = val;
+    this.filteredTracks = this.sort(this.filteredTracks);
+  }
+
+  filter: (track: Track) => boolean = ((track: Track) => {
+    if (this.search !== '') {
+      return track.title.toLowerCase().includes(this.search.toLowerCase());
+    }
+    return true;
+  });
+
+  sort: (tracks: Track[]) => Track[] = ((tracks: Track[]) => {
+    if (this.sortedAlphabetically) {
+      return tracks.sort((a, b) => a.title.toLowerCase().localeCompare(b.title.toLowerCase()));
+    } else {
+      return tracks.sort((a, b) => a.location.localeCompare(b.location));
+    }
+  });
+
+  constructor(
+    private dialog: MatDialog,
+    public settings: SettingsService,
+    private library: LibraryService
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.tracks) {
+      this.filteredTracks = this.sort(changes.tracks.currentValue.filter(this.filter));
+    }
+  }
+
+  trackByURL(index: number, track: Track) {
+    return track.url;
+  }
+
+  openDetailsDialog(track: Track) {
+    const dialogRef = this.dialog.open(DetailsComponent, {
+      // maxWidth: '500px',
+      data: { track: track }
+    });
+    dialogRef.afterClosed().subscribe(() => {});
+  }
+
+  scrollTo(letter: string) {
+    const scrollOptions: ScrollIntoViewOptions = { block: 'start', inline: 'nearest', behavior: 'smooth' };
+    if (letter === '#') {
+      this.list.nativeElement.getElementsByClassName('track')[0].scrollIntoView(scrollOptions);
+      return;
+    }
+    const tracks: Element[] = Array.from(this.list.nativeElement.getElementsByClassName('track'));
+    const elem = tracks.find(track => {
+      return track.getElementsByClassName('track-name')[0].textContent.toLowerCase().startsWith(letter.toLowerCase());
+    });
+    if (elem) {
+      elem.scrollIntoView(scrollOptions);
+    }
+  }
+
+  trackClicked(track: Track) {
+    this.next.emit();
+    this.library.setPlaylist(this.tracks.filter(this.filter));
+    this.library.playTrack(track);
+  }
+
+  addTrackToPlaylist(track: Track) {
+    this.library.addToCurrentPlaylist([track]);
+  }
+
+  addTracksToPlaylist() {
+    this.library.addToCurrentPlaylist(this.tracks.filter(this.filter));
+  }
+
+  playTrack(track: Track) {
+    this.library.playTrack(track);
+  }
+
+  playTrackNext(track: Track) {
+    this.library.playTrackNext(track);
+  }
+
+  isCurrentTrack(track: Track): boolean {
+    if (!this.currentTrack) {
+      return false;
+    } else {
+      return track.url === this.currentTrack.url;
+    }
+  }
+
+  isFavorite(track: Track): Observable<boolean> {
+    return this.library.isFavorite(track);
+  }
+
+  addToFavorites(track: Track) {
+    this.library.addToFavorites(track);
+  }
+
+  removeFromFavorites(track: Track) {
+    this.library.removeFromFavorites(track);
+  }
+
+}
