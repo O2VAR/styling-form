55a1de7815205d2e0eb625482c4e648d9ccb5e99
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-01-13 12:08:39 +0000

    Updated config file

--- web/src/app/library/reducers/albums.reducers.ts
+++ web/src/app/library/reducers/albums.reducers.ts
@@ -1,4 +1,13 @@
 import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
 import {Album} from '@app/model';
 import {AlbumsActionsUnion, AlbumsActionTypes} from '@app/library/actions/albums.actions';
-import {TracksActionsUnion, TracksActionTypes} from
+import {TracksActionsUnion, TracksActionTypes} from '@app/library/actions/tracks.actions';
+import {LibraryUtils} from '@app/library/library.utils';
+
+export const getAlbumId = (album: Album) => album.artist + '-' + album.title;
+
+/**
+ * State
+ */
+export interface State extends EntityState<Album> {
+  selectedIds: (stri
