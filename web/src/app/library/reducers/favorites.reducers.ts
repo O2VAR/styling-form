2927feb5662640e03232cc1278010951bca3e1d8
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-01-24 14:53:40 +0000

    ARM: at91: rm9200: remake the BGA as default version

--- web/src/app/library/reducers/favorites.reducers.ts
+++ web/src/app/library/reducers/favorites.reducers.ts
@@ -3,4 +3,18 @@ import {ImmutableTrack, toImmutable, Track} from '@app/model';
 import {FavoritesActionsUnion, FavoritesActionTypes} from '../actions/favorites.actions';
 
 export interface State {
-  favorites: Set<Immut
+  favorites: Set<ImmutableTrack>;
+}
+
+export const initialState: State = {
+  favorites: Set()
+};
+
+export function reducer(
+  state = initialState,
+  action: FavoritesActionsUnion
+): State {
+  switch (action.type) {
+
+    case FavoritesActionTypes.AddToFavorites: {
+      const tracksToAdd = action.payloa
