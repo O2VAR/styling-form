6aa4b35bee755d1b7e366a2834874f3a7841cc01
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-02-13 13:10:46 +0000

    New snapshot

--- web/src/app/library/reducers/tracks.reducers.ts
+++ web/src/app/library/reducers/tracks.reducers.ts
@@ -11,4 +11,20 @@ export interface State extends EntityState<ImmutableTrack> {
 }
 
 export const adapter: EntityAdapter<ImmutableTrack> = createEntityAdapter<ImmutableTrack>({
-  selectId: (track: Immu
+  selectId: (track: ImmutableTrack) => track.get('url'),
+  sortComparer: (a, b) => a.get('url').localeCompare(b.get('url')),
+});
+
+export const initialState: State = adapter.getInitialState({
+  error: '',
+  loading: false
+});
+
+/**
+ * Reducer
+ */
+export function reducer(
+  state = initialState,
+  action: TracksActionsUnion
+): State {
+  switch (a
