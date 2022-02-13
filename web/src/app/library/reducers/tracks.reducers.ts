6d333b392772fc4f1320d7d83f3bf17ceb1d55f1
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-02-13 12:52:57 +0000

    changes to documentation

--- web/src/app/library/reducers/tracks.reducers.ts
+++ web/src/app/library/reducers/tracks.reducers.ts
@@ -1,3 +1,14 @@
 import {createEntityAdapter, EntityAdapter, EntityState} from '@ngrx/entity';
 import {ImmutableTrack, toImmutable} from '@app/model';
-import {TracksActionsUnion, TracksActionTypes} from '@app/library/actions/t
+import {TracksActionsUnion, TracksActionTypes} from '@app/library/actions/tracks.actions';
+
+/**
+ * State
+ */
+export interface State extends EntityState<ImmutableTrack> {
+  error: string;
+  loading: boolean;
+}
+
+export const adapter: EntityAdapter<ImmutableTrack> = createEntityAdapter<ImmutableTrack>({
+  selectId: (track: Immu
