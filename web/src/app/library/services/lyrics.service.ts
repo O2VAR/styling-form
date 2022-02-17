37faaf0b3c39e685c8f911b12658c2fda7f46fc4
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-02-17 17:13:41 +0000

    block: fiops sync/async scale

--- web/src/app/library/services/lyrics.service.ts
+++ web/src/app/library/services/lyrics.service.ts
@@ -1,3 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpSocketClientService} from '@app/core/services/http-socket-client.service';
-import {Http
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {LyricsOptions, Track} from '@app/model';
+import {catchError, map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {CoreUtils} from '@app/core/core.utils';
+import {Store} from '@ngrx/store';
+import * as fromLibra
