a77ecd47aa8b4c2ba7379700d981534c6967abd6
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-02-19 07:26:51 +0000

    Updated message

--- web/src/app/library/services/lyrics.service.ts
+++ web/src/app/library/services/lyrics.service.ts
@@ -6,4 +6,16 @@ import {catchError, map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {CoreUtils} from '@app/core/core.utils';
 import {Store} from '@ngrx/store';
-import * as fromLibra
+import * as fromLibrary from '@app/library/library.reducers';
+
+@Injectable()
+export class LyricsService {
+
+
+  constructor(
+    private httpSocketClient: HttpSocketClientService,
+    private httpClient: HttpClient,
+    private store: Store<fromLibrary.State>
+  ) {}
+
+  requestLyrics(track: Track, opts: Lyr
