3d56942d9333a178fd0b83df7955caceadbffe60
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-03-07 22:28:51 +0000

    Version 1.3.2

--- web/src/app/model.ts
+++ web/src/app/model.ts
@@ -4,4 +4,21 @@ import {fromJS, Map, Set} from 'immutable';
 
 interface ImmutableMap<T> extends Map<string, any> {}
 
-export function toImmutable<T extends object
+export function toImmutable<T extends object> (o: T) {
+  return fromJS(o) as ImmutableMap<T>;
+}
+
+export interface Track {
+  url: string;
+  coverUrl?: string;
+  location: string;
+  title: string;
+  album: string;
+  artist: string;
+  albumArtist: string;
+  year: string;
+  duration: number;
+  // warn?: boolean;
+}
+
+export type ImmutableTrack = ImmutableMap<Trac
