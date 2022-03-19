de8e7bc9357dc6f13e618d6ba47923eccfa83609
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-03-19 09:15:00 +0000

    added command

--- web/src/app/my-music/my-music.component.ts
+++ web/src/app/my-music/my-music.component.ts
@@ -1,3 +1,15 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {LibraryService} from '@app/library/services/library.service';
-import {Observable} from 'rxjs
+import {Observable} from 'rxjs';
+import {Album, Artist, Track} from '@app/model';
+
+@Component({
+  selector: 'app-my-music',
+  template: `
+    <div class="my-music">
+      <mat-tab-group>
+        <mat-tab [label]="'Artists'">
+          <app-my-music-artists [artists]="artists$ | async">
+          </app-my-music-artists>
+        </mat-tab>
+       
