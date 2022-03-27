cbba697092d7ec4904fb2856346105bddb60c2ad
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-03-27 16:47:43 +0000

    bug 1005: Updated cmake file(s).

---
+++ web/src/app/player/components/controls.component.ts
@@ -0,0 +1,162 @@
+
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {Playlist, Track} from '@app/model';
+
+@Component({
+  selector: 'app-player-controls',
+  template: `
+    <div class="controls">
+      <button mat-button mat-icon-button
+              class="previous"
+              (click)="playPrevious.emit()"
+              [disabled]="!currentTrack || playlist.length <= 1">
+        <mat-icon>skip_previous</mat-icon>
+      </button>
+      <button mat-button mat-icon-button
+              class="playPause"
+              [disabled]="playlist.length === 0 && !currentTrack"
+              (click)="playing ? pause.emit() : (currentTrack ? resume.emit() : playNext.emit())">
+        <mat-icon>{{ playing ? 'pause' : 'play_arrow' }}</mat-icon>
+      </button>
+      <button mat-button mat-icon-button
+              class="next"
+              (click)="playNext.emit()"
+              [disabled]="!currentTrack || playlist.length <= 1">
+        <mat-icon>skip_next</mat-icon>
+      </button>
+      <button mat-button mat-icon-button
+              class="shuffle"
+              [color]="shuffle ? 'primary' : ''"
+              (click)="setShuffle.emit(!shuffle)">
+        <mat-icon>shuffle</mat-icon>
+      </button>
+      <button mat-button mat-icon-button
+              class="repeat"
+              [color]="repeat ? 'primary' : ''"
+              (click)="setRepeat.emit(!repeat)">
+        <mat-icon>repeat</mat-icon>
+      </button>
+      <button mat-button mat-icon-button
+              class="favorites"
+              [disabled]="!currentTrack"
+              [color]="isFavorite ? 'warn' : ''"
+              (click)="toggleFavorite.emit(currentTrack)">
+        <mat-icon>{{ isFavorite ? 'favorite' : 'favorite_border' }}</mat-icon>
+      </button>
+      <button mat-button mat-icon-button [matMenuTriggerFor]="playerMenu" class="more">
+        <mat-icon>more_horiz</mat-icon>
+      </button>
+      <mat-menu #playerMenu="matMenu">
+        <button mat-menu-item [matMenuTriggerFor]="LoadPlaylist" [disabled]="playlists.length === 0">
+          <mat-icon>playlist_play</mat-icon>
+          <span>Load playlist</span>
+        </button>
+        <button mat-menu-item [matMenuTriggerFor]="AddToPlaylist" [disabled]="playlists.length == 0">
+          <mat-icon>playlist_add</mat-icon>
+          <span>Add all to playlist</span>
+        </button>
+        <button mat-menu-item (click)="showInLibrary.emit()" [disabled]="playlist.length == 0">
+          <mat-icon>queue_music</mat-icon>
+          <span>Select in library</span>
+        </button>
+        <button mat-menu-item (click)="clearPlaylist.emit()" [disabled]="playlist.length == 0">
+          <mat-icon>delete</mat-icon>
+          <span>Clear playlist</span>
+        </button>
+        <button mat-menu-item (click)="savePlaylist.emit()" [disabled]="playlist.length == 0">
+          <mat-icon>save</mat-icon>
+          <span>Save playlist</span>
+        </button>
+      </mat-menu>
+      <mat-menu #LoadPlaylist="matMenu">
+        <ng-container *ngFor="let pl of playlists">
+          <button mat-menu-item (click)="loadPlaylist.emit(pl)">
+            <mat-icon>album</mat-icon>
+            {{pl.name}}
+          </button>
+        </ng-container>
+      </mat-menu>
+      <mat-menu #AddToPlaylist="matMenu">
+        <!--<button mat-menu-item disabled>-->
+        <!--<mat-icon>add</mat-icon>-->
+        <!--New playlist-->
+        <!--</button>-->
+        <!--<mat-divider></mat-divider>-->
+        <ng-container *ngFor="let pl of playlists">
+          <button mat-menu-item (click)="addAllToPlaylist.emit(pl)">
+            <mat-icon>album</mat-icon>
+            {{pl.name}}
+          </button>
+        </ng-container>
+      </mat-menu>
+      <div class="filler"></div>
+      <mat-slider
+        [disabled]="muted"
+        [value]="volume"
+        (input)="setVolume.emit($event.value)"
+        [max]="1"
+        [step]="0.01"
+        color="primary"
+        class="volume-slider"></mat-slider>
+      <button mat-button mat-icon-button
+              class="mute"
+              (click)="setMute.emit(!muted)">
+        <mat-icon *ngIf="!muted && volume > 0.50">volume_up</mat-icon>
+        <mat-icon *ngIf="!muted && volume > 0 && volume <= 0.50">volume_down</mat-icon>
+        <mat-icon *ngIf="!muted && volume == 0">volume_mute</mat-icon>
+        <mat-icon *ngIf="muted">volume_off</mat-icon>
+      </button>
+    </div>
+  `,
+  styles: [`
+    .controls {
+      padding: 0.5rem 1rem 0.5rem 1rem;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+    button:not(:last-child), mat-slider {
+      margin-right: 0.5rem;
+    }
+    .filler {
+      flex-grow: 1;
+    }
+    .volume-slider, .mute {
+      display: none;
+    }
+    @media screen and (min-width: 599px) {
+      .volume-slider, .mute {
+        display: unset;
+      }
+    }
+  `],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PlayerControlsComponent {
+
+  @Input() volume: number;
+  @Input() muted: boolean;
+  @Input() playing: boolean;
+  @Input() repeat: boolean;
+  @Input() shuffle: boolean;
+  @Input() isFavorite: boolean;
+  @Input() currentTrack: Track;
+  @Input() playlist: Track[];
+  @Input() playlists: Playlist[] = [];
+
+  @Output() setShuffle = new EventEmitter<boolean>();
+  @Output() setRepeat = new EventEmitter<boolean>();
+  @Output() playPrevious = new EventEmitter<void>();
+  @Output() playNext = new EventEmitter<void>();
+  @Output() pause = new EventEmitter<void>();
+  @Output() resume = new EventEmitter<void>();
+  @Output() toggleFavorite = new EventEmitter<Track>();
+  @Output() setVolume = new EventEmitter<number>();
+  @Output() setMute = new EventEmitter<boolean>();
+  @Output() showInLibrary = new EventEmitter<void>();
+  @Output() clearPlaylist = new EventEmitter<void>();
+  @Output() savePlaylist = new EventEmitter<void>();
+  @Output() loadPlaylist = new EventEmitter<Playlist>();
+  @Output() addAllToPlaylist = new EventEmitter<Playlist>();
+
+}
---
+++ web/src/app/player/components/header.component.ts
@@ -0,0 +1,90 @@
+
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {Track} from '@app/model';
+import {DomSanitizer} from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-player-header',
+  template: `
+    <div class="header">
+      <div class="cover"
+           [style]="getAvatarStyle(currentTrack)"
+           [class.no-art]="!currentTrack || !currentTrack.coverUrl">
+        <mat-icon *ngIf="!currentTrack || !currentTrack.coverUrl">music_note</mat-icon>
+      </div>
+      <div class="meta">
+        <span class="title" *ngIf="currentTrack">
+          {{ currentTrack.title }}
+        </span>
+        <span class="artist-album" *ngIf="currentTrack">
+          {{ currentTrack.artist }} • {{ currentTrack.album }}
+        </span>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .header {
+      padding: 1rem;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
+    .cover {
+      height: 100px;
+      min-width: 100px;
+      margin-right: 1rem;
+      background-size: cover;
+      box-sizing: border-box;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .cover mat-icon {
+      height: 50px;
+      width: 50px;
+      line-height: 50px;
+      font-size: 50px;
+    }
+    .meta {
+      display: flex;
+      flex-direction: column;
+    }
+    .title {
+      font-size: 25px;
+      line-height: 25px;
+      font-weight: 500;
+      margin-bottom: 1rem;
+    }
+    .artist-album {
+      font-size: 15px;
+      line-height: 15px;
+      font-weight: 300;
+    }
+    @media screen and (min-width: 599px) {
+      .cover {
+        height: 150px;
+        min-width: 150px;
+      }
+      .title {
+        font-size: 35px;
+        line-height: 35px;
+      }
+      .artist-album {
+        font-size: 20px;
+        line-height: 20px;
+      }
+    }
+  `],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PlayerHeaderComponent {
+
+  @Input() currentTrack: Track;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  getAvatarStyle(track: Track) {
+    return track && track.coverUrl ? this.sanitizer.bypassSecurityTrustStyle(`background-image: url("${track.coverUrl}")`) : '';
+  }
+
+}
