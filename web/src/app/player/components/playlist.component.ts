c8fa56a7c18cffa50762ae54bb32309dfc2b317d
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-03-27 16:50:06 +0000

    testing readme

--- web/src/app/player/components/playlist.component.ts
+++ web/src/app/player/components/playlist.component.ts
@@ -1,2 +1,15 @@
 import {
-  ChangeDetecti
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  QueryList,
+  SimpleChanges,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
+import {Track} from '@app/model';
+import {SelectionModel} from '@angular/cdk/collections
