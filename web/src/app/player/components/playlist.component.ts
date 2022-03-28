45a6fa4a8a04840647aed29b0017a2da35aeebe8
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-03-28 08:27:30 +0000

    Style footer

--- web/src/app/player/components/playlist.component.ts
+++ web/src/app/player/components/playlist.component.ts
@@ -12,4 +12,14 @@ import {
   ViewChildren
 } from '@angular/core';
 import {Track} from '@app/model';
-import {SelectionModel} from '@angular/cdk/collections
+import {SelectionModel} from '@angular/cdk/collections';
+import {CoreUtils} from '@app/core/core.utils';
+import {MatTable} from '@angular/material';
+
+@Component({
+  selector: 'app-player-playlist',
+  template: `
+    <em *ngIf="playlist.length === 0" class="empty">Playlist is empty</em>
+    <mat-table [dataSource]="playlist" #table>
+
+      <ng-cont
