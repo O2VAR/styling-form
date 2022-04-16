05cf12f1d929570e5bd8bff6328dc1a5870064f0
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-04-16 14:33:10 +0000

    loc

--- web/src/app/player/components/progress.component.ts
+++ web/src/app/player/components/progress.component.ts
@@ -1,1 +1,9 @@
-import {ChangeDetectionStrategy, Comp
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+
+// TODO refactor with app-progress
+@Component({
+  selector: 'app-player-progress',
+  template: `
+    <div class="progress">
+      <span class="time-elapsed">{{ currentTime ? (currentTime | sgTime) : '00:00' }}</span>
+      <mat-progress-ba
