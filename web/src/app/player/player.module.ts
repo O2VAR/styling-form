1d4114941e3d1fcea3fa1a62d91e84f820b1d05b
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-04-21 08:31:13 +0000

    [EPMCDLAB-780]: adding GCP for SSN

--- web/src/app/player/player.module.ts
+++ web/src/app/player/player.module.ts
@@ -1,4 +1,7 @@
 import {NgModule} from '@angular/core';
 import {SharedModule} from '@app/shared/shared.module';
 
-import {Playe
+import {PlayerComponent} from './player.component';
+import {PlayerHeaderComponent} from '@app/player/components/header.component';
+import {PlayerProgressComponent} from '@app/player/components/progress.component';
+import {PlayerControlsComponent} from '@app/player/components/controls.compon
