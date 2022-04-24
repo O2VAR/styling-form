70c422220091c1c501b3dbff623fb8dc095203bb
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-04-24 15:17:36 +0000

    Updated changes file.

--- web/src/app/player/player.module.ts
+++ web/src/app/player/player.module.ts
@@ -4,4 +4,19 @@ import {SharedModule} from '@app/shared/shared.module';
 import {PlayerComponent} from './player.component';
 import {PlayerHeaderComponent} from '@app/player/components/header.component';
 import {PlayerProgressComponent} from '@app/player/components/progress.component';
-import {PlayerControlsComponent} from '@app/player/components/controls.compon
+import {PlayerControlsComponent} from '@app/player/components/controls.component';
+import {PlayerPlaylistComponent} from '@app/player/components/playlist.component';
+
+export const COMPONENTS = [
+  PlayerComponent,
+  PlayerHeaderComponent,
+  PlayerProgressComponent,
+  PlayerControlsComponent,
+  PlayerPlaylistComponent
+];
+
+@NgModule({
+  imports: [
+    SharedModule
+  ],
+ 
