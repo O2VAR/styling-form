d9020de2eb42ad990afd6a02029fcfa71eadd1bf
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-05-20 21:50:13 +0000

    Update json

--- web/src/app/playlists/playlists.component.ts
+++ web/src/app/playlists/playlists.component.ts
@@ -103,4 +103,13 @@ import {ConfirmComponent} from '@app/shared/dialogs/confirm.component';
             <ng-container *ngFor="let cover of getCovers(item).slice(0, 16)">
               <div [style]="getStyle(cover)" class="cover">&nbsp;</div>
             </ng-container>
-            <mat-icon class="play-icon">play_circle
+            <mat-icon class="play-icon">play_circle_outline</mat-icon>
+          </div>
+          <span class="primary" [matTooltip]="item.name">{{ item.name }}</span>
+          <span class="secondary">{{ item.tracks.length }} songs</span>
+        </li>
+      </ul>
+      <mat-divider></mat-divider>
+      <h2>
+        Suggested Albums
+        
