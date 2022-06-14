a69b4dc3e10b1002c6557eb21fbcfbaa81435118
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-06-14 21:00:39 +0000

    docs(changelog): update changelog [ci skip]

--- web/src/app/settings/components/library-folders.component.ts
+++ web/src/app/settings/components/library-folders.component.ts
@@ -5,4 +5,10 @@ import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@
   template: `
     <mat-list>
       <mat-list-item class="hover" *ngFor="let folder of folders">
-   
+        <mat-icon mat-list-icon>folder_open</mat-icon>
+        <span matLine>{{ folder }}</span>
+        <button class="close" mat-button mat-icon-button (click)="removeFolder.emit(folder)">
+          <mat-icon>close</mat-icon>
+        </button>
+      </mat-list-item>
+      <mat-list-item *ngIf="loadi
