23a0ac8603b2c4cb660f03996778da19b69695de
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-07-31 17:12:30 +0000

    Revert "Fix tests"

--- web/src/app/settings/reducers/lyrics.reducers.ts
+++ web/src/app/settings/reducers/lyrics.reducers.ts
@@ -1,1 +1,16 @@
-import {Sett
+import {SettingsActionsUnion, SettingsActionTypes} from '../settings.actions';
+import {LyricsOptions} from '@app/model';
+
+/**
+ * State
+ */
+export interface State extends LyricsOptions {
+  useService: boolean;
+  services: {
+    wikia: boolean;
+    lyricsOvh: boolean;
+  };
+  automaticSave: boolean;
+}
+
+const initialState: State = {
