46ce0fa90d2daa75a8eaf8b9bdfd9f977e8e2acc
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-08-07 11:38:52 +0000

    Updated models

--- web/src/app/settings/settings.component.ts
+++ web/src/app/settings/settings.component.ts
@@ -1,3 +1,9 @@
 import {ChangeDetectionStrategy, Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {MatDialog, MatSlideToggleChange, MatSnackBar} from '@an
+import {MatDialog, MatSlideToggleChange, MatSnackBar} from '@angular/material';
+import {Observable} from 'rxjs';
+import {map, share} from 'rxjs/operators';
+
+import {FolderComponent} from '../shared/dialogs/folder/folder.component';
+import {HttpSocketClientService} from '../core/services/http-socket-client.service';
+import {SettingsService} from './services/settings.serv
