15ac18ca4c17fef01cee79e2c6bc4282915e3546
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-11-06 10:19:09 +0000

    JSON updated by the Owl Bot

--- web/src/app/settings/settings.reducers.ts
+++ web/src/app/settings/settings.reducers.ts
@@ -1,1 +1,9 @@
-import {ActionReducerMap, createFeatureSelector, createSelecto
+import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store';
+
+import * as fromLibraryFolders from './reducers/libray.reducers';
+import * as fromLyrics from './reducers/lyrics.reducers';
+import * as fromRoot from '@app/app.reducers';
+
+export interface SettingsState {
+  library: fromLibraryFolders.State;
+  lyrics: fromLyr
