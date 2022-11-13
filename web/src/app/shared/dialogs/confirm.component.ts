5d2df08dcf647b621195e9fe0d2f489f6bea44bd
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-11-13 22:04:48 +0000

    Fix module

--- web/src/app/shared/dialogs/confirm.component.ts
+++ web/src/app/shared/dialogs/confirm.component.ts
@@ -5,4 +5,13 @@ import {MAT_DIALOG_DATA} from '@angular/material';
   selector: 'app-confirm',
   template: `
     <h3 mat-dialog-title>{{ data.title }}</h3>
-    <div mat-dialog-content class="ma
+    <div mat-dialog-content class="mat-typography">
+      <p [innerHtml]="data.message"></p>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button [mat-dialog-close]="false">Cancel</button>
+      <button mat-button [mat-dialog-close]="true">Yes</button>
+    </div>
+  `,
+  styles: [``],
+  changeDetection: ChangeDetectionStrategy.OnPu
