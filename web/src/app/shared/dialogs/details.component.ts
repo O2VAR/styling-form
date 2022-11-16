949af5d92eb13e5260055483ae0c56e8ba5eaa2e
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-11-16 17:12:00 +0000

    Updated license year.

--- web/src/app/shared/dialogs/details.component.ts
+++ web/src/app/shared/dialogs/details.component.ts
@@ -1,3 +1,12 @@
 import {ChangeDetectionStrategy, Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
-// import {
+// import {CoreUtils} from '@app/core/core.utils';
+
+@Component({
+  selector: 'app-details',
+  template: `
+    <h3 mat-dialog-title>Details of "{{ data.track.title }}"</h3>
+    <div mat-dialog-content>
+      <mat-grid-list cols="2" rowHeight="fit" gutterSize="1rem">
+        <mat-grid-tile [colspan]="2">
+  
