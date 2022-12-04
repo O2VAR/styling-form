ece906a6826838d163eea876cb535ce0ed837cf7
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-12-04 18:55:00 +0000

    Initial Import

--- web/src/app/shared/dialogs/dialogs.module.ts
+++ web/src/app/shared/dialogs/dialogs.module.ts
@@ -1,2 +1,9 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angu
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+
+import {MaterialModule} from '@app/shared/material/material.module';
+import {FormsModule} from '@angular/forms';
+import {ConfirmComponent} from './confirm.component';
+import {DetailsComponent} from './details.component';
+import {FolderComponent} from './folder/folder.
