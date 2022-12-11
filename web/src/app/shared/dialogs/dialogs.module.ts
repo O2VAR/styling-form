bb20077073f9031045ab0664367669b9acd74505
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-12-11 05:33:01 +0000

    emp

--- web/src/app/shared/dialogs/dialogs.module.ts
+++ web/src/app/shared/dialogs/dialogs.module.ts
@@ -6,4 +6,12 @@ import {MaterialModule} from '@app/shared/material/material.module';
 import {FormsModule} from '@angular/forms';
 import {ConfirmComponent} from './confirm.component';
 import {DetailsComponent} from './details.component';
-import {FolderComponent} from './folder/folder.
+import {FolderComponent} from './folder/folder.component';
+import {PlaylistsDialogComponent} from './playlists-dialog.component';
+import {NewPlaylistDialogComponent} from '@app/shared/dialogs/new-playlist-dialog.component';
+import {InfoComponent} from '@app/shared/dialogs/info.component';
+
+export const COMPONENTS = [
+  ConfirmComponent,
+  DetailsComponent,
+  FolderComponen
