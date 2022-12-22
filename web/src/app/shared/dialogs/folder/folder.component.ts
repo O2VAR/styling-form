a1212ef5e56ace9a783d1a46c75d8e9ac9da0b8c
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-12-22 12:02:49 +0000

    Remove deprecated methods.

--- web/src/app/shared/dialogs/folder/folder.component.ts
+++ web/src/app/shared/dialogs/folder/folder.component.ts
@@ -4,4 +4,22 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.component.html',
-  styleUrls: ['.
+  styleUrls: ['./folder.component.scss']
+})
+export class FolderComponent implements OnInit {
+
+  folder: string;
+
+  constructor(
+    public dialogRef: MatDialogRef<FolderComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  ngOnInit() {
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
+
+}
