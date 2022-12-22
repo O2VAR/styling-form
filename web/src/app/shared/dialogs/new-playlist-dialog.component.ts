b176bd8a810bba980bb3cc4a5e388650473525f3
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-12-22 16:14:49 +0000

    Post release tag bump

--- web/src/app/shared/dialogs/new-playlist-dialog.component.ts
+++ web/src/app/shared/dialogs/new-playlist-dialog.component.ts
@@ -1,1 +1,10 @@
-i
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'app-new-playlist-dialog',
+  template: `
+    <h3 mat-dialog-title>New Playlist</h3>
+    <div mat-dialog-content>
+      <mat-form-field>
+        <
