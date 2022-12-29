f6c6cf3b4ef6844ed0ee987dbb6f641802a745e3
O2VAR <102681345+O2VAR@users.noreply.github.com>
2022-12-29 07:51:36 +0000

    Rebuilding the website

--- web/src/app/shared/dialogs/new-playlist-dialog.component.ts
+++ web/src/app/shared/dialogs/new-playlist-dialog.component.ts
@@ -7,4 +7,9 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
     <h3 mat-dialog-title>New Playlist</h3>
     <div mat-dialog-content>
       <mat-form-field>
-        <
+        <input matInput placeholder="Playlist name" [(ngModel)]="playlistName" spellcheck="false">
+      </mat-form-field>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button (click)="cancel()">Cancel</button>
+      <button mat-button [mat-dialog-close]="playlistName" [disabled]="!pla
