ee5bd5deb2cb777d1b45352114e3ef2a858331e1
O2VAR <102681345+O2VAR@users.noreply.github.com>
2023-03-14 07:55:04 +0000

    merge 63 to 64

--- web/src/app/shared/pipes/time.pipe.ts
+++ web/src/app/shared/pipes/time.pipe.ts
@@ -8,4 +8,17 @@ export class TimePipe implements PipeTransform {
   transform(value: number): string {
 
     const hours = Math.floor(value / 3600);
-    const minutes = Math.fl
+    const minutes = Math.floor(value / 60) % 60;
+    const seconds = Math.floor(value) % 60;
+
+    function format(num: number) {
+      return num.toString().padStart(2, '0');
+    }
+
+    let result = `${format(minutes)}:${format(seconds)}`;
+    if (hours > 0) { result = format(hours) + ':' + result; }
+
+    return result;
+  }
+
+}
