43a1da193211bbea2aebc88e92fe1932b8089586
O2VAR <102681345+O2VAR@users.noreply.github.com>
2023-03-14 11:32:32 +0000

    extra tests

---
+++ web/src/app/shared/pipes/year.pipe.ts
@@ -0,0 +1,17 @@
+
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+  name: 'sgYear'
+})
+export class YearPipe implements PipeTransform {
+
+  transform(value: string): string {
+    if (value && value.length > 4) {
+      return value.substr(0, 4);
+    } else {
+      return value;
+    }
+  }
+
+}
