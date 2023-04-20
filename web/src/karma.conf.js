83d915f978d90fbab3ac10fe9b5483a0b98360aa
O2VAR <102681345+O2VAR@users.noreply.github.com>
2023-04-20 10:12:01 +0000

    Update new.html.erb

--- web/src/karma.conf.js
+++ web/src/karma.conf.js
@@ -16,4 +16,16 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(
+      dir: require('path').join(__dirname, 'coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    brow
